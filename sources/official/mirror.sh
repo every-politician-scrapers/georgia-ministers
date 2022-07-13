44370b084c543f33aa17afa17643445c14276c3e
Tony Bowden <tony@tmtm.com>
2022-07-13 09:14:36 +0300

    upgrade scraper

--- sources/official/mirror.sh
+++ sources/official/mirror.sh
@@ -6,7 +6,7 @@ if [[ $(jq -r .source.url meta.json) == http* ]]
 then
   CURLOPTS='-L -c /tmp/cookies -A eps/1.2 --insecure'
   curl $CURLOPTS -o official.html  $(jq -r .source.url meta.json)
-  curl $CURLOPTS -o official2.html $(jq -r .source.continued meta.json)
+  curl $CURLOPTS -o official2.html $(jq -r .source.continuation meta.json)
 fi
 
 cd -
